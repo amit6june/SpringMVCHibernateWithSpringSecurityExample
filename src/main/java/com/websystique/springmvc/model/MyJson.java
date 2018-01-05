32bca5386b3bf683ef58d911770e06cc6677e5d9
Amit Mishra <kumar.amit.mishra@gmail.com>
2018-01-06 09:04:07 +1100

    first commit

---
+++ src/main/java/com/websystique/springmvc/model/MyJson.java
@@ -0,0 +1,42 @@
+package com.websystique.springmvc.model;
+
+
+public class MyJson {
+
+
+	private String series;
+
+
+	private String username;
+	
+
+	private String token;
+	
+
+	public String getSeries() {
+		return series;
+	}
+
+	public void setSeries(String series) {
+		this.series = series;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+
+
+}
